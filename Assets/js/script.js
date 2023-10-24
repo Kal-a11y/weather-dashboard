ca46ee14c650f252115e6fa1137fb05adb885bbd
Kaila <kailaamia@gmail.com>
2023-10-24 00:44:47 -0500

    Added search button event and showWeather() base

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,6 +9,14 @@ const weatherApiKey = '923a850bd61fc38e956f15c51f45cfe2';
 const weatherApiUrlBase = 'https://api.openweathermap.org/data/3.0/onecall/timemachine?appid=' + weatherApiKey;
 const locationByNameUrlBase = 'http://api.openweathermap.org/geo/1.0/direct?appid=' + weatherApiKey;
 
+searchBtnElement.on('click', showWeather)
+
+function showWeather(event){
+    event.preventDefault();
+    
+    cityName = searchBarElement.val();
+    
+}
 //when a city is typed into a search bar
 //collect that value
 //call openweather geocoding api to turn name into cordinates
