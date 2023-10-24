669117a44b32b65a709186a60caf97ab8ecbce4e
Kaila <kailaamia@gmail.com>
2023-10-24 00:36:57 -0500

    Added geolocation api

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,6 +7,7 @@ const weatherApiKey = '923a850bd61fc38e956f15c51f45cfe2';
 
 //Api call urls
 const weatherApiUrlBase = 'https://api.openweathermap.org/data/3.0/onecall/timemachine?appid=' + weatherApiKey;
+const locationByNameUrlBase = 'http://api.openweathermap.org/geo/1.0/direct?appid=' + weatherApiKey;
 
 //when a city is typed into a search bar
 //collect that value
