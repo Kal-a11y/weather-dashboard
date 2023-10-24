515e6c367e66f13102c964a2f97e6d786fac3c8e
Kaila <kailaamia@gmail.com>
2023-10-23 19:52:09 -0500

    Added search elements to js file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,8 @@
+//Page elements
+const searchBarElement = $('#city-search-bar');
+const searchBtnElement = $('#city-search-button')
+
+//Api keys
 const weatherApiKey = '923a850bd61fc38e956f15c51f45cfe2';
 const weatherApiUrl = 'https://api.openweathermap.org/data/3.0/onecall?'
     + 'lat=' + {lat} 
