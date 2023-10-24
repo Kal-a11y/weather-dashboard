f7d8d29259e6087adf07e0df7f6104ef7e545542
Kaila <kailaamia@gmail.com>
2023-10-23 19:35:00 -0500

    Edited weatherApiUrl

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,9 @@
 const weatherApiKey = '923a850bd61fc38e956f15c51f45cfe2';
+const weatherApiUrl = 'https://api.openweathermap.org/data/3.0/onecall?'
+    + 'lat=' + {lat} 
+    + '&lon=' + {lon}
+    + '&exclude=' + {part}
+    + '&appid=' + weatherApiKey;
 
 
 
