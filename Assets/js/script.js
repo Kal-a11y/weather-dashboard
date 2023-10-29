6ce80b439dcbb9cdc1e94e91d53d42c89afde787
Kaila <kailaamia@gmail.com>
2023-10-29 16:11:15 -0500

    Formated document

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,83 +11,83 @@ const locationByNameUrlBase = 'http://api.openweathermap.org/geo/1.0/direct?appi
 
 searchBtnElement.on('click', showWeather)
 
-function showWeather(event){
+function showWeather(event) {
     event.preventDefault();
-    
+
     cityName = searchBarElement.val();
-    
-    if (cityName === ''){
+
+    if (cityName === '') {
         console.log('TODO: Alert or toast to show invalid input')
         return
     }
-     showTodayWeather(cityName);
-     showForcastWeather(cityName);
+    showTodayWeather(cityName);
+    showForcastWeather(cityName);
 
-     
-     addCityHistoryBtn(cityName) ;
-    }
 
-    function addCityHistoryBtn(){
-        let cityBtn = $('<button>', {type: 'button', class:'btn btn-outline-primary historyData col-12', id:`${cityName.toLowerCase().replace(' ','')}Btn`}).text(cityName.toUpperCase())
-     
-        $('#history-list').append(cityBtn)
-        let historyList = $('#history-list').children()
+    addCityHistoryBtn(cityName);
+}
 
-        for (let index = 0; index < historyList.length; index++) {
-            let lastIndex = historyList.length - 1;
-            
-            if (historyList[index].id === historyList[lastIndex].id ){
-                if (index !== lastIndex){
-                    historyList[lastIndex].remove();
-                }
+function addCityHistoryBtn() {
+    let cityBtn = $('<button>', { type: 'button', class: 'btn btn-outline-primary historyData col-12', id: `${cityName.toLowerCase().replace(' ', '')}Btn` }).text(cityName.toUpperCase())
+
+    $('#history-list').append(cityBtn)
+    let historyList = $('#history-list').children()
+
+    for (let index = 0; index < historyList.length; index++) {
+        let lastIndex = historyList.length - 1;
+
+        if (historyList[index].id === historyList[lastIndex].id) {
+            if (index !== lastIndex) {
+                historyList[lastIndex].remove();
             }
-            
         }
-        
+
     }
-    
-    function showTodayWeather(cityName){
-        fetch(weatherApiUrlBase + '&q=' + cityName)
-        .then(function(response){
+
+}
+
+function showTodayWeather(cityName) {
+    fetch(weatherApiUrlBase + '&q=' + cityName)
+        .then(function (response) {
             return response.json()
         })
-        .then(function(data){
+        .then(function (data) {
             $('#city-name').text(cityName.toUpperCase())
             $('#today-date').text(new Date(data.dt * 1000).toDateString())
-            $('#weather-visual').attr('src',`https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+            $('#weather-visual').attr('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
             $('#temp-value').text(data.main.temp + ' °F');
             $('#wind-value').text(data.wind.speed + ' MPH')
             $('#humidity-value').text(data.main.humidity + ' %');
         })
 }
-function showForcastWeather(cityName){
+function showForcastWeather(cityName) {
     $('#forcast-box').empty();
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherApiKey}&units=imperial`)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        for (let i = 0; i < 5; i++) {
-            const myTest = $(`<div class="card col-2" id="forcast-day-${i}"><p><strong>${new Date(data.list[i*8].dt * 1000).toDateString()}</strong></p><img src="https://openweathermap.org/img/w/${data.list[i*8].weather[0].icon}.png"></img><ul><li>Temp: ${data.list[i*8].main.temp + ' °F'}</li><li>Wind: ${data.list[i*8].wind.speed + ' MPH'}</li><li>Humidity: ${data.list[i*8].main.humidity + ' %'}</li></ul></div>`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            for (let i = 0; i < 5; i++) {
+                const myTest = $(`<div class="card col-2" id="forcast-day-${i}"><p><strong>${new Date(data.list[i * 8].dt * 1000).toDateString()}</strong></p><img src="https://openweathermap.org/img/w/${data.list[i * 8].weather[0].icon}.png"></img><ul><li>Temp: ${data.list[i * 8].main.temp + ' °F'}</li><li>Wind: ${data.list[i * 8].wind.speed + ' MPH'}</li><li>Humidity: ${data.list[i * 8].main.humidity + ' %'}</li></ul></div>`)
 
-            $('#forcast-box').append(myTest);
+                $('#forcast-box').append(myTest);
 
-            
-        }
-    })
+
+            }
+        })
 }
 
 //When search is clicked, Then add button with city name in list
 //When city is already in list, Then dont add button of same city
-    //loop throuch children in list
-        //if button.text() === current cityName then dont add button
-        //else add button
+//loop throuch children in list
+//if button.text() === current cityName then dont add button
+//else add button
 //When city button is clicked, Then display city data that was stored
-    //When button is created then give id = cityName.toLowerCase + 'Btn and class = historyData
-    //create #history-list.on('click',.historyData, function(event))
-        //set let name = this.text()
-        //call storage for name
-        //display data
+//When button is created then give id = cityName.toLowerCase + 'Btn and class = historyData
+//create #history-list.on('click',.historyData, function(event))
+//set let name = this.text()
+//call storage for name
+//display data
 
 
